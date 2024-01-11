630884cb07c0c093485dfcfdbb3bc74256ac6f49
EAguirre <aguirreernesto25@gmail.com>
2024-01-11 19:57:58 -0300

    Listo Principal

--- RiseUp - Portafolio/src/App.jsx
+++ RiseUp - Portafolio/src/App.jsx
@@ -1,7 +1,7 @@
 import NavBar from "./componentes/NavBar"
+import Principal from "./componentes/Principal"
 // import Contacto from "./componentes/Contacto"
 // import Preguntas from "./componentes/Preguntas"
-// import Principal from "./componentes/Principal"
 // import Servicios from "./componentes/Servicios"
 // import Complete from "./componentes/Complete"
 
@@ -9,8 +9,8 @@ export default function App () {
   return (
     <div>
       <NavBar/>
-      {/* <Principal/>
-      <Preguntas/>
+      <Principal/>
+      {/* <Preguntas/>
       <Servicios/>
       <Contacto/>
       <Complete/> */}
--- RiseUp - Portafolio/src/componentes/Principal.jsx
+++ RiseUp - Portafolio/src/componentes/Principal.jsx
@@ -1,22 +1,25 @@
-export default function Principal () {
-    return (
-        <div className="flex-col overflow-hidden self-stretch relative flex min-h-[837px] w-full justify-center items-center px-16 py-12 max-md:max-w-full max-md:px-5">
+export default function Principal() {
+  return (
+    <div className="items-center flex-col relative flex">
+
+      <img
+        src="../../public/fondos/hombre-usando-tecnologia-inteligente-maqueta-psd-tableta-digital.jpg"
+        className="absolute h-full w-full"
+      />
+
+      <div className="relative flex w-[625px] max-w-full flex-col mt-10 mb-20">
         <img
-          src="../../public/fondos/hombre-usando-tecnologia-inteligente-maqueta-psd-tableta-digital.jpg"
-          className="absolute h-full w-full object-cover object-center inset-0"
+          src="../../public/logo Riseup/logo png.png"
+          className="aspect-[3] object-contain"
         />
-        <span className="relative flex w-[624px] max-w-full flex-col items-stretch mt-36 mb-16 max-md:my-10">
-          <img
-            src="../../public/logo Riseup/logo png.png"
-            className="aspect-[2] object-contain object-center w-[507px] overflow-hidden self-center max-w-full"
-          />
-          <div className="justify-center text-white text-center text-6xl font-medium max-md:max-w-full max-md:text-4xl">
-            Elevamos tu presencia, multiplicamos tus ventas
-          </div>
-          <span className="justify-center text-white text-center text-4xl font-medium w-[392px] shadow-sm bg-green-600 self-center max-w-full items-center mt-16 px-16 py-5 rounded-3xl max-md:mt-10 max-md:px-5">
-            ¡Subir de nivel!
-          </span>
-        </span>
+        <div className="justify-center text-white text-center text-5xl font-medium">
+          Elevamos tu presencia, multiplicamos tus ventas
+        </div>
+
+        <div className="justify-center text-white text-center text-4xl font-medium w-[392px] bg-green-600 self-center mt-16 py-5 rounded-3xl cursor-pointer hover:bg-green-700">
+          ¡Subir de nivel!
+        </div>
       </div>
-    )
+    </div>
+  )
 }
