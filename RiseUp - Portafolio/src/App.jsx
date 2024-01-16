0a0866bc6f8acabc9b907708729aed4f954c2b9b
EAguirre <aguirreernesto25@gmail.com>
2024-01-15 22:10:14 -0300

    Sigue contacto

--- RiseUp - Portafolio/src/App.jsx
+++ RiseUp - Portafolio/src/App.jsx
@@ -4,7 +4,8 @@ import Preguntas2 from "./componentes/Preguntas2"
 // import Preguntas from "./componentes/Preguntas"
 // import Servicios from "./componentes/Servicios"
 import Servicios2 from "./componentes/Servicios2"
-// import Contacto from "./componentes/Contacto"
+import Contacto from "./componentes/Contacto"
+import Contacto2 from "./componentes/Contacto2"
 // import Complete from "./componentes/Complete"
 
 export default function App () {
@@ -16,8 +17,9 @@ export default function App () {
       {/* <Preguntas/> */}
       <Servicios2/>
       {/* <Servicios/> */}
-      {/* <Contacto/>
-      <Complete/> */}
+      <Contacto/>
+      <Contacto2/>
+      {/* <Complete/> */}
     </div>
   )
 }
--- RiseUp - Portafolio/src/componentes/Contacto.jsx
+++ RiseUp - Portafolio/src/componentes/Contacto.jsx
@@ -1,12 +1,11 @@
 export default function Contacto() {
     return (
-        <div>
+        <div className="flex flex-wrap justify-center">
             <div className="bg-zinc-300 self-center flex w-[916px] shrink-0 max-w-full h-0.5 flex-col mt-32 max-md:mt-10" />
             <div className="justify-center text-white text-center text-6xl self-center max-w-[816px] mt-24 max-md:max-w-full max-md:text-4xl max-md:mt-10">
                 <span className="font-medium">¿Necesitas </span>
                 <span className="font-medium">elevar tu presencia</span>
                 <span className="font-medium">?</span>
-                <br />
             </div>
             <span className="justify-center text-white text-center text-4xl font-medium w-[551px] shadow-sm bg-green-600 self-center max-w-full items-center mt-28 px-16 py-6 rounded-3xl max-md:mt-10 max-md:px-5">
                 Contactanos
