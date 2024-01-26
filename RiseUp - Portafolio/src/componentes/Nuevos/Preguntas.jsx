cd9945b68a345cd1ee5d940e60531d8989303225
EAguirre <aguirreernesto25@gmail.com>
2024-01-26 11:30:07 -0300

    Preguntas Listo

--- RiseUp - Portafolio/src/componentes/Nuevos/Preguntas.jsx
+++ RiseUp - Portafolio/src/componentes/Nuevos/Preguntas.jsx
@@ -6,17 +6,17 @@ export default function Preguntas() {
         <h1 className="justify-center text-white text-center text-3xl md:text-5xl pt-2 pb-3 mt-5">
           ¿Por qué <span className="font-medium">RiseUp</span>?
         </h1>
-        <div className="flex flex-col md:flex-row mx-4 gap-1 items-center md:items-start">
-          <div className="w-full md:w-3/5 text-white text-lg md:text-4xl text-opacity-70 text-center md:text-left md:mt-20">
+        <div className="flex flex-col md:flex-row mx-4 gap-1 items-center md:items-start md:mt-5">
+          <div className="w-full md:w-3/5 text-white text-lg md:text-4xl text-opacity-70 text-center md:text-left md:mt-4">
             En <span className="text-white">RiseUp</span> fusionamos la creatividad del diseño con la excelencia en el desarrollo web. <br />  <br />Ofrecemos soluciones digitales que no solo son visualmente impactantes, sino también funcionales y efectivas.
           </div>
           <img
-            src="../../../dist/Letras Nuevas/up222.png"
+            src="../../../dist/Letras Nuevas/up2222.png"
             className="object-contain object-center overflow-x-auto grow w-full md:w-2/5 hidden md:block"
           />
         </div>
       </div>
-      <div className="">
+      <div className="mt-5">
         <h1 className="justify-center text-white text-center text-3xl md:text-5xl pt-2 pb-3">
           <span className="font-medium">Sobre</span> nosotros
         </h1>
